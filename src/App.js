d4863f754aaef8bc631fe27e480f00e5e2da4ed5
Prashant Sethi <theseth.prashant@gmail.com>
2020-06-30 00:48:56 +0530

    First commit, uploading to Github

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Header } from "./components/layout/Header";
+import { Content } from "./components/layout/Content";
+import { ProjectsProvider, SelectedProjectProvider } from "./context";
+
+import "./App.scss";
+
+export const App = () => {
+  return (
+    <SelectedProjectProvider>
+      <ProjectsProvider>
+        <div className="App">
+          <Header />
+          <Content />
+        </div>
+      </ProjectsProvider>
+    </SelectedProjectProvider>
+  );
+};
